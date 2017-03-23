069b73ce6c1a6d755ae39a54147e20d4be49f870
wolfj123 <wolfj@post.bgu.ac.il>
2017-03-23 11:17:04 +0200

    Fixed some issues with the files previously pushed

--- MarketClient/UserInterface.cs
+++ MarketClient/UserInterface.cs
@@ -12,7 +12,38 @@ namespace MarketClient
     {
         static void Main()
         {
+            //Default values:
             String url = "http://ise172.ise.bgu.ac.il";
+            String username = "user36";
+            String key = "tmp"; //How do you write this ?!?!
+            String token = SimpleCtyptoLibrary.CreateToken(username, key);
+          
+            
+            /*
+            * String key = -----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQCuGdcd1NEIrVWC / bjTAWQUfjhC6yJMQF / udGKvO7Yp + Dlnxbhk
+1gNQrmD9ICjyEOGrKaubCJnrI0Zcjvfml3n9FhJUJeD6XrE6dSY0znUoNc8juBae
+e1dy / 29plyXuQOft1fL0MsckfcSWZzJ / 64Cpdh515oGeUqQjiZdI6Y / vmQIDAQAB
+AoGARiHipgG0suogKERMz7MfvaGayFov1seX3VbE6hIDr6Rue38KaJRNgZK9PzpV
+RC3Iukpu9mTgm / f5wA9XjWw3lzGOt0qt08dK / 63ESA6e1Bbe2 + S3zIqn1JM8SJ6J
+ZGys1YCvezEadh00Ia16dZhixVrvtaHERsUHi5dzGP0EYoECQQDHWaJRYEo10lbz
+57ajQvTp / oDHGJNjd5l9L1EAQO4 + VHc1fIpgYye9g / msPIX6bqpWaTSAMoR7phl5
+TuVy9LxPAkEA35NfWoEDbSYw1yjezMwVm2EORYOd18EjGTRYQHSkQbXiyp2NMNKy
+DHzSZao7iw38CwYVejo6VNYoyxkQJVUTlwJABjYawqJXbZniL7NWk3uwmeHeLVXs
+sbq2Q5pH0dQ0GCkVlcsNnLc6M8N68gzot8be89ZPVnc8fYXNYWQ97fkGLQJAPUT9
+1KeWcMsOh2hD5ovnP / WRG6u + Dep32 + hkZwWQHhHiXPRgRQj4kkOCxSmpt6nVcI / y
+QtTCN42ZEE + GBTUTcQJBAMafJ6ike5spiGCkx2ZzHh9IUu9H9TJ4u5KNxJiP1BIS
+rxv9gh / KJgqOXc / YV3RG1FuQdflRy3ZvQutoIrznyKA =
+-----END RSA PRIVATE KEY---- - 
+
+    */
+
+
+
+            Credentials userInfo = new Credentials(url, username, token);
+            
+
+
 
         }
 
@@ -23,15 +54,15 @@ namespace MarketClient
 
 
 
-    class Interperator
+    public class Interperator
     {
-        private Credentials credentials;
+        private Credentials userInfo;
         private Poster poster;
 
-        public Interperator(Credentials credentials)
+        public Interperator(Credentials userInfo)
         {
-            this.credentials = credentials;
-            this.poster = new Poster(credentials);
+            this.userInfo = userInfo;
+            this.poster = new Poster(userInfo);
         }
 
 
@@ -42,12 +73,12 @@ namespace MarketClient
             {
                 case 1 : //Buy
 
-                    String[] names = new String[] {"price", "commodity", "amount"};
-                    int[] inputs = new int[3];
-                    AskInput(names, inputs);
+                    InputSlot inputSlots = new InputSlot("price", "commodity", "amount");
+                    int[] inputs = AskInput(inputSlots);
+                    
+                    //int response = poster.SendBuyRequest(inputs[0], inputs[1], inputs[2]);
 
-
-                    poster.SendBuyRequest(inputs[0], inputs[1], inputs[2]);
+                    //Display.Write(response);
                     break;
 
 
@@ -62,14 +93,29 @@ namespace MarketClient
         }
 
 
-        private int[] AskInput(String[] names, int[] inputs)
+        private int[] AskInput(InputSlot inputSlots)
         {
+            for(int i = 0; i<inputSlots.names.Length; i= i+1)
+            {
+                Display.Write(inputSlots.names[i] + "?");
+                String currentInput = Console.ReadLine();
 
+                //If the user submitted non-integer string
+                while (!isNumeric(currentInput))
+                {
+                    Display.MustBeInt();
+                    currentInput = Console.ReadLine();
+                }
+
+            }
+
+            return inputSlots.data;
         }
-
-        private bool CheckType(String input, InputSlot inputSlot)
+        
+        static public bool isNumeric(String s)
         {
-
+            int i = 0;
+            return int.TryParse(s, out i);
         }
 
     }
@@ -94,19 +140,19 @@ namespace MarketClient
 
 
     public class InputSlot
-    {
-        public String name;
-        public int data;
+    {   
+        public String[] names;
+        public int[] data;
 
-        public InputSlot(String name, int data)
+        public InputSlot(String[] name)
         {
-
-
+            this.names = names;
+            this.data = new int[names.Length];
         }
     }
 
 
-    public class Display
+    public static class Display
     {
 
         static public void Write(String s)
@@ -126,7 +172,12 @@ namespace MarketClient
 
         static public void Unknown()
         {
-            Console.WriteLine("Unknown Command. Write 'help' if you need assistance.");
+            Console.WriteLine("Unknown Command");
+        }
+
+        static public void MustBeInt()
+        {
+            Console.WriteLine("Input must be integer!");
         }
 
         static public void Welcome()

058465c8c148613405e5de102d9b83bc08353e38
wolfj123 <wolfj@post.bgu.ac.il>
2017-03-23 23:56:21 +0200

    .

--- MarketClient/BL/Poster.cs
+++ MarketClient/BL/Poster.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MarketClient.DAL;
 using MarketClient.Utils;
 using MarketClient.DataEntries;
 

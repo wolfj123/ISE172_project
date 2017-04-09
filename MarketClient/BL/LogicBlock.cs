cbc5a6723c2c16ab7d447c06da336b358e62dcf3
wolf123 <wolfj@post.bgu.ac.il>
2017-04-09 13:47:14 +0300

    .

---
+++ MarketClient/BL/LogicBlock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketClient.BL.milestone1
+{
+    internal class LogicBlock
+    {
+
+
+
+
+
+
+    }
+}

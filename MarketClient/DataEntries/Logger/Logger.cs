235ed84599a2a0bc240b8c941ad1ceedbfd1fc17
hadasat <atiyah@post.bgu.ac.il>
2017-04-15 18:15:18 +0300

    logging

---
+++ MarketClient/DataEntries/Logger/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketClient.DataEntries.Logger
+{
+    class Logger
+    {
+        public Logger() {
+            string path = @"https://github.com/wolfj123/ISE172_project.git/Log";
+            FileStream filestream = new FileStream(@"C:/MyFolder/Text.txt", FileMod.Append, FileAccess.Write);
+            filestream.close();
+        }
+
+        public static void logging(IMarketRequest request)
+        {
+            RequestType request = request.getType();
+            switch (request)
+            {
+                case "buy":
+
+            }
+        }
+    }
+}

82461fd59e5c66ac8cd8133e47c82b8513396c5e
wolfj123 <wolfj@post.bgu.ac.il>
2017-04-20 12:08:00 +0300

    added testTimer for ama

--- MarketClientTest/AMATimerTest.cs
+++ MarketClientTest/AMATimerTest.cs
@@ -5,13 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarketClient.BL;
 
 namespace MarketClientTest
 {
     [TestClass]
-    class AMATimerTest
+    public class AMATimerTest
     {
+        [TestMethod]
+        public void TestTimer()
+        {
+            LogicBlock testLogic = new PrintLogic();
+            AMA amaTest = new AMA(10, 10000);
+            amaTest.add(testLogic);
+            amaTest.enable(true);
 
+            System.Threading.Thread.Sleep(10000);
+            amaTest.enable(false);
+        }
 
     }
+
+
+    public class PrintLogic : LogicBlock
+    {
+        public PrintLogic()
+        {
+            setRepeat(true);
+        }
+        public override object run()
+        {
+            Trace.WriteLine("test");
+            return null;
+        }
+    }
+
 }

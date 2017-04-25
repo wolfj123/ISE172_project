9bd181148da528f7bdc2a550893cac7e29efd50c
shiraev <shiraev@post.bgu.ac.il>
2017-04-25 13:15:41 +0300

    1

---
+++ MarketClient/PL-BL/InterperatorPB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketClient.PL_BL
+{
+    static class InterperatorPB
+    {
+        public static IMarketResponse sendReq(Request req)
+        {
+            return null;
+        }
+    }
+}

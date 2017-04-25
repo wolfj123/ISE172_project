755bb420ea4a9a61f0a6b8e0df9547a661b2accb
shiraev <shiraev@post.bgu.ac.il>
2017-04-25 13:38:37 +0300

    2

--- MarketClient/PL-BL/Request.cs
+++ MarketClient/PL-BL/Request.cs
@@ -8,7 +8,7 @@ namespace MarketClient.PL_BL
 {
     public class Request
     {
-        public RequestType type;
+        public String type;
     }
 
     public class BuySellRequest : Request
@@ -38,68 +38,69 @@ namespace MarketClient.PL_BL
             this.amount = amount;
             this.commodity = commodity;
             this.price = price;
-            this.type = RequestType.buy;
+            this.type = "buy";
 
         }
     }
-        public class SellRequest : Request
+
+    public class SellRequest : Request
+    {
+
+        public int commodity;
+        public int amount;
+        public int price;
+
+        public SellRequest(int commodity, int amount, int price)
         {
+            this.amount = amount;
+            this.commodity = commodity;
+            this.price = price;
+            this.type = "sell";
 
-            public int commodity;
-            public int amount;
-            public int price;
-
-            public SellRequest(int commodity, int amount, int price)
-            {
-                this.amount = amount;
-                this.commodity = commodity;
-                this.price = price;
-                this.type = RequestType.sell;
-
-            }
-        }
-
-        public class CancelRequest : Request
-        {
-            public int id;
-
-            public CancelRequest(int id)
-            {
-                this.id = id;
-                this.type = RequestType.cancel;
-            }
-        }
-
-        public class QueryBuySellRequest : Request
-        {
-            public int id;
-
-            public QueryBuySellRequest(int id)
-            {
-                this.id = id;
-                this.type = RequestType.buySellQ;
-            }
-        }
-
-        public class QueryMarketRequest : Request
-        {
-            public int commodity;
-
-            public QueryMarketRequest(int commodity)
-            {
-                this.commodity = commodity;
-                this.type = RequestType.marketQ;
-            }
-        }
-
-        public class QueryUserRequest : Request
-        {
-            public QueryUserRequest()
-            {
-                this.type = RequestType.userQ;
-            }
         }
     }
+
+    public class CancelRequest : Request
+    {
+        public int id;
+
+        public CancelRequest(int id) 
+        {
+            this.id = id;
+            this.type = "cancelBuySell";
+        }
+    }
+
+    public class QueryBuySellRequest : Request
+    {
+        public int id;
+        
+        public QueryBuySellRequest (int id) 
+        {
+            this.id = id;
+            this.type = "queryBuySell";
+        }
+    }
+
+    public class QueryMarketRequest : Request
+    {
+        public int commodity;
+
+        public QueryMarketRequest(int commodity) 
+        {
+            this.commodity = commodity;
+            this.type = "queryMarket";
+        }
+    }
+
+    public class QueryUserRequest : Request 
+    {
+        public QueryUserRequest () 
+        {
+            this.type = "queryUser";
+        }
+    }
+}
 
 
 

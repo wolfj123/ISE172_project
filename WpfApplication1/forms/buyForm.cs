29ed214a0a58e0692dfe11b1ea8f6e2ac8b21a11
shiraev <shiraev@post.bgu.ac.il>
2017-04-26 13:24:56 +0300

    11

--- WpfApplication1/forms/buyForm.cs
+++ WpfApplication1/forms/buyForm.cs
@@ -19,7 +19,7 @@ namespace WpfApplication1.forms
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int[] toAdd = formsMethod.listGet();
+            int[] toAdd = formsMethod.listGet();//
             for (int i = 0; i < toAdd.Length - 1; i++) {
                 comboBox1.Items.Add(toAdd[i]);
             }

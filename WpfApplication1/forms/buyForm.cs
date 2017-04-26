28c802dcda714f91099a5172307804992bc5fcae
shiraev <shiraev@post.bgu.ac.il>
2017-04-26 13:01:01 +0300

    1

---
+++ WpfApplication1/forms/buyForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WpfApplication1.forms
+{
+    public partial class buyForm : Form
+    {
+        public buyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int[] toAdd = formsMethod.listGet();
+            for (int i = 0; i < toAdd.Length - 1; i++) {
+                comboBox1.Items.Add(toAdd[i]);
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int[] toAdd = formsMethod.listGet();
+            for (int i = 0; i < toAdd.Length - 1; i++)
+            {
+                comboBox1.Items.Add(toAdd[i]);
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int[] toAdd = formsMethod.listGet();
+            for (int i = 0; i < toAdd.Length - 1; i++)
+            {
+                comboBox1.Items.Add(toAdd[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApplication1/forms/formsMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.forms
+{
+    static class formsMethod
+    {
+        public static int[] listGet()
+        {
+            int[] o = new int[0];
+            return o;
+        }
+    }
+}

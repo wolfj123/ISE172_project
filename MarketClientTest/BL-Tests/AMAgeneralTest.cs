13b451c7122284000873ebd136e1030304bb221a
wolf123 <wolfj@post.bgu.ac.il>
2017-05-01 00:08:37 +0300

    trying to add log4net to track the amaTest

--- MarketClientTest/BL-Tests/AMAgeneralTest.cs
+++ MarketClientTest/BL-Tests/AMAgeneralTest.cs
@@ -2,18 +2,21 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MarketClient.BL;
 using MarketClient.PL_BL;
+using log4net;
 
 namespace MarketClientTest.BL_Tests
 {
     [TestClass]
     public class AMAgeneralTest
     {
+        private static ILog myLogger = LogManager.GetLogger("fileLogger");
+
+
         [TestMethod]
         public void TestMethod1()
         {
             AMA testAMA = new DefaultAMA();
-
-
+            testAMA.enable(true);
 
         }
     }

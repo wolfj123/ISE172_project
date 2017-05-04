0a875c1a43079f96ce3e1f587e6c192a95920d45
wolfj123 <wolfj@post.bgu.ac.il>
2017-05-04 16:15:30 +0300

    implemented InterperatorPB

--- MarketClient/PL-BL/InterperatorPB.cs
+++ MarketClient/PL-BL/InterperatorPB.cs
@@ -9,9 +9,46 @@ namespace MarketClient.PL_BL
 {
     static class InterperatorPB
     {
+        static private ICommunicator comm = new Communicator();
+
         public static IMarketResponse sendReq(Request req)
         {
-            return null;
+            switch (req.type)
+            {
+                case "buy":
+                    BuyRequest buyReq = (BuyRequest)req;
+                    return comm.SendBuyRequest(buyReq.price, buyReq.commodity, buyReq.amount);
+
+                case "sell":
+                    SellRequest sellReq = (SellRequest)req;
+                    return comm.SendSellRequest(sellReq.price, sellReq.commodity, sellReq.amount);
+
+                case "cancelBuySell":
+                    CancelRequest cancelReq = (CancelRequest)req;
+                    return comm.SendCancelBuySellRequest(cancelReq.id);
+
+                case "queryBuySell":
+                    QueryBuySellRequest queryReq = (QueryBuySellRequest)req;
+                    return comm.SendQueryBuySellRequest(queryReq.id);
+
+                case "queryMarket":
+                    QueryMarketRequest queryMarketReq = (QueryMarketRequest)req;
+                    return comm.SendQueryMarketRequest(queryMarketReq.commodity);
+
+                case "queryUser":
+                    return comm.SendQueryUserRequest();
+
+
+                //ADD case for new request
+                default:
+                    return null;
+            }
+        }
+
+
+        public static void addLogic(int commodity, int price, int amount)
+        {
+            //TODO
         }
     }
 }

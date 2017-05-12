68a6cd7feeee5e6012465b51a5d90b1f1daf6db6
wolf123 <wolfj@post.bgu.ac.il>
2017-05-12 15:13:18 +0300

    added logging

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MarketClient.BL;
 using MarketClient.PL_BL;
+using log4net;
 
 namespace WpfApplication1
 {
@@ -24,6 +25,8 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private static ILog myLogger = LogManager.GetLogger("fileLogger");
 
         private bool runningAMA;
         private ICommunicator comm;
@@ -37,18 +40,26 @@ namespace WpfApplication1
             ama = new DefaultAMA(comm);
             userAma = new UserAMA();
             runningAMA = false;
+
+
+            myLogger.Info("\nMainWindow initialized");
         }
 
 
         private void buyButton_Click(object sender, RoutedEventArgs e)
         {
+            myLogger.Info("User opened BuyForm");
             //buy
             forms.buyForm userInput = new forms.buyForm();
             userInput.Show();
+
+            
         }
 
         private void sellButton_Click(object sender, RoutedEventArgs e)
         {
+            myLogger.Info("User opened SellForm");
+
             forms.sellForm userInput = new forms.sellForm();
             //buySellReq userInput = new buySellReq("sell");
             userInput.Show();
@@ -56,13 +67,17 @@ namespace WpfApplication1
 
         private void cancelButton_Click(object sender, RoutedEventArgs e)
         {
+            myLogger.Info("User opened CancelForm");
+
             forms.cancelForm userInput = new forms.cancelForm();
             //intReq userInput = new intReq("cancel");
             userInput.Show();
         }
 
-        private void rquestQueryButton_Click(object sender, RoutedEventArgs e)
+        private void requestQueryButton_Click(object sender, RoutedEventArgs e)
         {
+            myLogger.Info("User opened idStatusForm");
+
             forms.idStatusForm userInput = new forms.idStatusForm();
             //intReq userInput = new intReq("buySellQ");
             userInput.Show();
@@ -70,6 +85,8 @@ namespace WpfApplication1
 
         private void marketQueryButton_Click(object sender, RoutedEventArgs e)
         {
+            myLogger.Info("User opened ComStatForm");
+
             forms.comStatForm userInput = new forms.comStatForm();
             userInput.Show();
         }
@@ -79,11 +96,14 @@ namespace WpfApplication1
             QueryUserRequest req = new QueryUserRequest();
             IMarketResponse res = InterperatorPB.sendRequest(req);
             MessageBox.Show(this, res.ToString());
+
+            //myLogger.Info("User opened SellForm");
         }
 
         private void amaButton_Click(object sender, RoutedEventArgs e)
         {
             runningAMA = !runningAMA;       //toggle runningAMA
+            myLogger.Info("User set Default AMA enabled to " + runningAMA);
             ama.enable(runningAMA);         //toggle ama
 
             enableControls(!runningAMA);     //enable/disable mainwindow
@@ -96,12 +116,14 @@ namespace WpfApplication1
 
         private void amaString_Click(object sender, RoutedEventArgs e)
         {
+            myLogger.Info("User viewed Default AMA rules list");
             MessageBox.Show(this, ama.ToString());
         }
 
         private void userAMAbutton_Click(object sender, RoutedEventArgs e)
         {
             runningAMA = !runningAMA;       //toggle runningAMA
+            myLogger.Info("User set User AMA enabled to " + runningAMA);
             userAma.enable(runningAMA);         //toggle ama
 
             enableControls(!runningAMA);     //enable/disable mainwindow
@@ -110,30 +132,33 @@ namespace WpfApplication1
             if (runningAMA)     //update text
                 userAMAbutton.Content = "Stop user AMA";
             else userAMAbutton.Content = "Run user AMA";
-
         }
 
         private void userAMAString_Click(object sender, RoutedEventArgs e)
         {
+            myLogger.Info("User viewed User AMA rules list");
             MessageBox.Show(this, userAma.ToString());
         }
 
         private void clearLogicButton_Click(object sender, RoutedEventArgs e)
         {
+            myLogger.Info("User cleared AMA logic");
             userAma.clearLogic();
             MessageBox.Show(this, "User AMA has been cleared of all rules!");
         }
 
         private void addLogicButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            myLogger.Info("User opened SellForm");
+            AMA.AddLogicForm addLogicForm = new AMA.AddLogicForm();
+            addLogicForm.userAma = this.userAma;
         }
 
 
 
         private void enableControls(bool mode)
         {
-
+            myLogger.Info("MainWindow controls set to " + mode);
 
 
 

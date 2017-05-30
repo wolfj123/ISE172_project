d40628e953381ab7c478e050bed894f3e4eee9d8
wolf123 <wolfj@post.bgu.ac.il>
2017-05-30 18:50:18 +0300

    seperated Communicator classes

---
+++ MarketClient/BL/Server-Communication/ICommunicator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarketClient.Utils;
+using MarketClient.PL_BL;
+using log4net;
+
+namespace MarketClient.BL
+{
+    public interface ICommunicator
+    {
+
+        IMarketResponse SendBuyRequest(int price, int commodity, int amount);
+
+        IMarketResponse SendSellRequest(int price, int commodity, int amount);
+
+        IMarketResponse SendQueryBuySellRequest(int id);
+
+        IMarketResponse SendQueryUserRequest();
+
+        IMarketResponse SendQueryMarketRequest(int commodity);
+
+        IMarketResponse SendCancelBuySellRequest(int id);
+
+        IMarketResponse SendQueryAllMarketRequest();
+
+        IMarketResponse SendQueryAllUserRequest();
+    }
+}

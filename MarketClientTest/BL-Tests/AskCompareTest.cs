c0a53bce25d0e78a551e4fd00ecc9cf05693c31c
wolf123 <wolfj@post.bgu.ac.il>
2017-06-03 21:14:57 +0300

    .

--- MarketClientTest/BL-Tests/AskCompareTest.cs
+++ MarketClientTest/BL-Tests/AskCompareTest.cs
@@ -24,7 +24,7 @@ namespace MarketClientTest
             //int commodity = 0;
             MQCommodity qmarket = new MQCommodity();
             qmarket.ask = "10";
-            ICommunicator comm = new CommStubStaticReturn(null, null, null, null, qmarket, null);
+            ICommunicator comm = new CommStubStaticReturn(null, null, null, null, qmarket, null,null,null);
             AdvancedAMA agent = new AdvancedAMA(1, 1000, comm);
 
             //AlgoProcess process = new AlgoProcess(agent, comm, commodity);

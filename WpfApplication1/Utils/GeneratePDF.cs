7a8f73d64b269efd1a5db5091bac13aae046a56e
shiraev <shiraev@post.bgu.ac.il>
2017-06-05 12:59:21 +0300

    .

---
+++ WpfApplication1/Utils/GeneratePDF.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace MarketClient.Utils
+{
+    static class GeneratePDF
+    {
+        public static void ceate()
+        {
+            FileStream fs = new FileStream("Chapter1_Example1.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+            Document doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+        }
+    }
+}

d882fce002521eb4d46e3c3f42328df5a867d1c7
wolf123 <wolfj@post.bgu.ac.il>
2017-06-08 12:15:29 +0300

    .

--- MarketClient/BL/Server-Communication/HTTP-Clients/SimpleCryptoHTTPClient.cs
+++ MarketClient/BL/Server-Communication/HTTP-Clients/SimpleCryptoHTTPClient.cs
@@ -9,41 +9,10 @@ namespace MarketClient.BL
 {
     public class SimpleCryptoHTTPClient : SimpleHTTPClient
     {
+        private Int64 nonceInt;
 
-        Int64 nonceInt;
-
-
+        //TODO: have only 1 instance
         public SimpleCryptoHTTPClient(){
-            /*
-            Int64 sign = -1;
-            Int64 year = (DateTime.Today.Year - 2016);//* (int)Math.Pow(10,19);
-            year = year * (Int64)Math.Pow(10, 19);
-
-            Int64 test = (Int64)Math.Pow(10, 19);
-            Int64 month = DateTime.Today.Month;//* (int)Math.Pow(10, 17);
-            month = month * (Int64)Math.Pow(10, 17);
-
-            Int64 day = DateTime.Today.Day;//* (int)Math.Pow(10, 15);
-            day = day * (Int64)Math.Pow(10, 15);
-
-            Int64 hour = DateTime.Now.Hour;//* (int)Math.Pow(10, 13);
-            hour = hour * (Int64)Math.Pow(10, 13);
-
-            Int64 minutes = DateTime.Now.Minute;//* (int)Math.Pow(10, 11);
-            minutes = minutes * (Int64)Math.Pow(10, 11);
-
-            Int64 seconds = DateTime.Now.Second;//* (int)Math.Pow(10, 9);
-            seconds = seconds * (Int64)Math.Pow(10, 9);
-
-            Int64 miliseconds = DateTime.Now.Millisecond;// * (int)Math.Pow(10, 6);
-            miliseconds = miliseconds * (Int64)Math.Pow(10, 6);
-
-            nonceInt = year + month + day + hour + minutes + seconds + miliseconds;
-
-            Trace.WriteLine(nonceInt);
-            */
-
-
             string concat = "";
             concat += (DateTime.Today.Year - 2016).ToString();
             concat += DateTime.Today.ToString("MM");
@@ -61,8 +30,6 @@ namespace MarketClient.BL
 
         public override string SendPostRequest<T1>(string url, string user_base, string privateKey, T1 item)
         {
-            //string nonce = SimpleCtyptoLibrary.createNonce();
-
             string nonce = nonceInt.ToString();
             string user = user_base +"_"+ nonce;
             string token = SimpleCtyptoLibrary.CreateToken(user, privateKey);
@@ -83,7 +50,6 @@ namespace MarketClient.BL
                 }
                 finally { }
                 return responseContent;
-
             }
         }
 

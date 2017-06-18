dfb32654d86a688992065385e6a4a8305a6a5954
shiraev <shiraev@post.bgu.ac.il>
2017-06-18 11:39:10 +0300

    BV

--- WpfApplication1/forms/userStatForm.cs
+++ WpfApplication1/forms/userStatForm.cs
@@ -66,10 +66,12 @@ namespace WpfApplication1.forms
                 QueryUserRequest req1 = new QueryUserRequest();
                 IMarketResponse resp1 = InterperatorPB.sendRequest(req1);
                 //QueryAllBuySellRequest req2 = new QueryAllBuySellRequest();
-                List<MQReqWrapper> resp2 = InterperatorPB.sendAllUserRequest();
-              
+                //List<MQReqWrapper> resp2 = InterperatorPB.sendAllUserRequest();
+                
                 doc1.Open();
                 doc1.Add(new iTextSharp.text.Paragraph(resp1.ToString()));
+                //doc1.Add(new iTextSharp.text.Paragraph(InterperatorPB.sendAllUserRequest().ToString()));
+
                 doc1.Close();
             }
         }

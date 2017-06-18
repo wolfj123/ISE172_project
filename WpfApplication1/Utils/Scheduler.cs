1186ae1287c2776f6be8226686a6e2646c303279
shiraev <shiraev@post.bgu.ac.il>
2017-06-18 18:08:44 +0300

    //

---
+++ WpfApplication1/Utils/Scheduler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentScheduler;
+
+namespace WpfApplication1.Utils
+{
+    class Scheduler
+    {
+        Registry registry = new Registry();
+    }
+}

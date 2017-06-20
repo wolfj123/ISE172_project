3b2276a0ad5a64952fcf7cf50c891d85a04519ff
shiraev <shiraev@post.bgu.ac.il>
2017-06-20 14:31:47 +0300

    v

--- WpfApplication1/forms/userStatForm.cs
+++ WpfApplication1/forms/userStatForm.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MarketClient.PL_BL;
 using log4net;
-using GUIprim.Utils;
+
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.IO;
@@ -46,19 +46,19 @@ namespace WpfApplication1.forms
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-            dlg.FileName = "Document"; // Default file name
-            dlg.DefaultExt = ".pdf"; // Default file extension
-            dlg.Filter = "PDF documents (.pdf)|*.pdf"; // Filter files by extension 
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.FileName = "Current Status"; // Default file name
+            dialog.DefaultExt = ".pdf"; // Default file extension
+            dialog.Filter = "PDF documents (.pdf)|*.pdf"; // Filter files by extension 
 
             // Show open file dialog box
-            Nullable<bool> result = dlg.ShowDialog();
+            Nullable<bool> result = dialog.ShowDialog();
 
             // Process open file dialog box results 
             if (result == true)
             {
                 // Open document
-                string res = dlg.FileName;
+                string res = dialog.FileName;
 
                 var doc1 = new Document();
                 PdfWriter.GetInstance(doc1, new FileStream(res, FileMode.Create));
@@ -69,7 +69,10 @@ namespace WpfApplication1.forms
                 List<MQReqWrapper> resp2 = InterperatorPB.sendAllUserRequest();
                 
                 doc1.Open();
+                doc1.Add(new iTextSharp.text.Paragraph("CURRENT USER STATUS:"));
+                doc1.Add(new iTextSharp.text.Paragraph("\n"));
                 doc1.Add(new iTextSharp.text.Paragraph(resp1.ToString()));
+                doc1.Add(new iTextSharp.text.Paragraph("\n"));
                 foreach (var element in resp2)
                 {
                     doc1.Add(new iTextSharp.text.Paragraph(element.ToString()));
